25ed0ea5d1d8e7f7791039d1d10e15d9d6eaeb3a
Edmund Horner <ejrh00@gmail.com>
2023-09-27 23:05:19 +1300

    Run simulations in parallel and time them

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,7 @@
+use std::num::NonZeroUsize;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::thread;
+use std::time::SystemTime;
 use rand::seq::SliceRandom;
 
 use othello::ai::{evaluate_immediate, Score};
@@ -36,14 +40,32 @@ fn simulate_one_game() -> Game {
 fn main() {
     println!("Othello");
 
-    let mut total_score = 0;
+    let total_score = AtomicUsize::new(0);
+    let games_run: AtomicUsize = AtomicUsize::new(0);
+    const NUM_GAMES: usize = 100000;
+    let t0 = SystemTime::now();
+    let num_threads = thread::available_parallelism()
+        .unwrap_or(NonZeroUsize::new(1).expect("1 != 0"));
+    let num_threads = num_threads.get();
+    thread::scope(|s| {
+        for _ in 0..num_threads {
+            s.spawn(|| {
+                let mut thread_total_score: usize = 0;
+                for _ in 0..(NUM_GAMES / num_threads) {
+                    let game = simulate_one_game();
+                    let score = evaluate_immediate(&game);
+                    // println!("Game: score={}\n{:?}", score, &game);
+                    thread_total_score += score as usize;
+                }
+                total_score.fetch_add(thread_total_score, Ordering::Relaxed);
+                games_run.fetch_add(NUM_GAMES / num_threads, Ordering::Relaxed);
+            });
+        }
+    });
+    let total_score = total_score.into_inner();
+    let games_run = games_run.into_inner();
+    println!("Simulating {} games on {} threads took {:?}",
+             games_run, num_threads, t0.elapsed().expect("no time travel"));
 
-    for _ in 0..1000 {
-        let game = simulate_one_game();
-        let score = evaluate_immediate(&game);
-        // println!("Game: score={}\n{:?}", score, &game);
-        total_score += score;
-    }
-
-    println!("Total score={}", total_score);
+    println!("Average score={:2.2}", total_score as f64 / games_run as f64);
 }
