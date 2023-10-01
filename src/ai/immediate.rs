a433c27ea22d811292d0c6ef97573b130a5159fa
Edmund Horner <ejrh00@gmail.com>
2023-10-01 19:40:14 +1300

    Extract iteration over moves looking for the best one into a common function

--- src/ai/immediate.rs
+++ src/ai/immediate.rs
@@ -1,4 +1,4 @@
-use crate::ai::{AI, evaluate_immediate, Score};
+use crate::ai::{AI, evaluate_immediate, pick_best_move, Score};
 use crate::game::{Game, Move};
 
 pub struct ImmediateAI {
@@ -6,24 +6,11 @@ pub struct ImmediateAI {
 
 impl AI for ImmediateAI {
     fn choose_move(&self, game: &Game) -> Option<Move> {
-
         fn evaluate_move(game: &Game, mov: Move) -> Score {
             let game2 = game.apply(mov);
             evaluate_immediate(&game2)
         }
 
-        let mut moves = game.valid_moves();
-        let mut best_move = moves.next();
-        if best_move.is_some() {
-            let mut best_score = evaluate_move(game, best_move.unwrap());
-            for m in moves {
-                let new_score =  evaluate_move(game, m);
-                if new_score > best_score {
-                    best_score = new_score;
-                    best_move = Some(m);
-                }
-            }
-        }
-        best_move
+        pick_best_move(game, evaluate_move)
     }
 }
--- src/ai/mod.rs
+++ src/ai/mod.rs
@@ -27,6 +27,23 @@ pub fn evaluate_immediate(game: &Game) -> Score {
     score
 }
 
+pub fn pick_best_move<F>(game: &Game, evaluate_move: F) -> Option<Move>
+where F: Fn(&Game, Move) -> Score {
+    let mut moves = game.valid_moves();
+    let mut best_move = moves.next();
+    if best_move.is_some() {
+        let mut best_score = evaluate_move(game, best_move.unwrap());
+        for m in moves {
+            let new_score =  evaluate_move(game, m);
+            if new_score > best_score {
+                best_score = new_score;
+                best_move = Some(m);
+            }
+        }
+    }
+    best_move
+}
+
 pub trait AI {
     fn choose_move(&self, game: &Game) -> Option<Move>;
 }
