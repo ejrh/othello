93a555fe832f5f6f0db48f3062abb0d90fd64f64
Edmund Horner <ejrh00@gmail.com>
2023-11-05 22:51:04 +1300

    Add a binary that can run lots of random games and count the moves in each

---
+++ src/bin/count-moves.rs
@@ -0,0 +1,44 @@
+use rand::prelude::SliceRandom;
+
+use othello::game::Game;
+
+fn run_one_game() -> (usize, usize, usize, Option<Game>) {
+    let mut turns: usize = 0;
+    let mut total_moves = 0;
+    let mut max_moves = 0;
+    let mut max_moves_game = None;
+    let mut game = Game::new();
+
+    loop {
+        let moves: Vec<_> = game.valid_moves().collect();
+        if moves.is_empty() { break }
+
+        let num = moves.len();
+        turns += 1;
+        total_moves += num;
+        if num > max_moves {
+            max_moves = num;
+            max_moves_game = Some(game.clone());
+        }
+
+        let mov = moves.choose(&mut rand::thread_rng()).expect("at least one move");
+        game = game.apply(*mov);
+    }
+
+    (turns, total_moves, max_moves, max_moves_game)
+}
+
+fn main() {
+    let mut total_max_moves = 0;
+    let mut total_max_moves_game = None;
+    for game_no in 0..1000000 {
+        let (_turns, _total_moves, max_moves, max_moves_game) = run_one_game();
+        //println!("turns {} moves {} max {}", turns, total_moves, max_moves);
+        if max_moves >= total_max_moves {
+            total_max_moves = max_moves;
+            total_max_moves_game = max_moves_game;
+            println!("{}: {}", game_no, total_max_moves);
+            println!("{:?}", total_max_moves_game.unwrap());
+        }
+    }
+}
